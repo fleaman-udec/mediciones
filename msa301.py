35b218ed7e721f93b82cc21f4cf1789fe75d4f82
Dr.-Ing. Félix Leaman <102315822+fleaman-udec@users.noreply.github.com>
2022-05-17 15:55:21 -0400

    Update msa301.py

--- msa301.py
+++ msa301.py
@@ -1,1 +1,14 @@
+import time
+import board
+import busio
+import adafruit_msa301
 
+i2c = busio.I2C(scl=board.GP1, sda=board.GP0)
+msa = adafruit_msa301.MSA301(i2c)
+msa.data_rate = adafruit_msa301.DataRate.RATE_125_HZ
+
+count = 1
+while count < 20:
+    count += 1
+    print(msa.acceleration)
+    time.sleep(0.1)
